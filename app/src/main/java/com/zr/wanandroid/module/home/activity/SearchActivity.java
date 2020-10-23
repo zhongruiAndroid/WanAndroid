c571eedb5b12c1c9ad0874c3d6a41c0b92f75b0d
zhongrui <zhongrui@021.com>
2020-10-23 20:17:37 +0800

    搜索页面

---
+++ app/src/main/java/com/zr/wanandroid/module/home/activity/SearchActivity.java
@@ -0,0 +1,28 @@
+package com.zr.wanandroid.module.home.activity;
+
+import android.view.View;
+
+import com.zr.wanandroid.R;
+import com.zr.wanandroid.base.BaseActivity;
+
+public class SearchActivity extends BaseActivity {
+    @Override
+    public int getContentView() {
+        return R.layout.search_act;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void onNoDoubleClick(View v) {
+
+    }
+}
