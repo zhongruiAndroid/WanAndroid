dc0ae12cc10993635bf9e21ddc2c3a9bec598746
zhongrui <zhongrui@021.com>
2020-11-13 17:08:55 +0800

    收藏

---
+++ app/src/main/java/com/zr/wanandroid/module/my/activity/CollectActivity.java
@@ -0,0 +1,27 @@
+package com.zr.wanandroid.module.my.activity;
+
+import android.view.View;
+
+import com.zr.wanandroid.base.BaseActivity;
+
+public class CollectActivity extends BaseActivity {
+    @Override
+    public int getContentView() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void onNoDoubleClick(View v) {
+
+    }
+}
